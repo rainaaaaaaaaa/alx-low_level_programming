39cdae69d41bfb5cb16c25492a850aa90b682a76
Rain <rainymorning0000@gmail.com>
2023-03-20 13:24:06 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -5,29 +5,32 @@
 
 
 /**
- * main prints if the last digit of n is greater than 5: the string and is greater than 5
- * if the last digit of n is 0: the string and is 0
- * if the last digit of n is less than 6 and not 0: the string and is less than 6 and not 0
+ * main -  prints if the last digit of n is greater than 5: the string and is greater than 5,
+ * if the last digit of n is 0: the string and is 0,
+ * if the last digit of n is less than 6 and not 0: the string and is less than 6 and not 0.
  * Return: Always (back)
  */
 
 int main(void)
 
 {
-	int n;
+	int n, lastdgt;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (last digit of n > 5)
+	lastdgt = n % 10;
+
+	if (lastdgt > 5)
 	{
-	printf("%d and is greater than 5\n", n);
+	printf("Last digit of %d is %d and is greater than 5\n", n, lastdgt);
 	}
-	if else (last digit of n == 0)
+	else if (lastdgt == 0)
 	{
-	printf("%d and is 0\n", n);
+	printf("Last digit of %d is %d and is 0\n", n, lastdgt);
 	}
-	while (last digit of 0 < n < 6)
+	else if (lastdgt < 6 && lastdgt != 0)
 	{
-	printf("%d and is less than 6 and not 0\n", n);
+	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastdgt);
 	}
 	return (0);
+}
