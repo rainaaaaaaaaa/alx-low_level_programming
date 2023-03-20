ff1649522a9fd6f37f5949beffecd696554fec2c
Rain <rainymorning0000@gmail.com>
2023-03-20 13:27:35 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -5,9 +5,11 @@
 
 
 /**
- * main -  prints if the last digit of n is greater than 5: the string and is greater than 5,
+ * main -  prints if the last digit of n is greater than 5:
+ * the string and is greater than 5,
  * if the last digit of n is 0: the string and is 0,
- * if the last digit of n is less than 6 and not 0: the string and is less than 6 and not 0.
+ * if the last digit of n is less than 6 and not 0:
+ * the string and is less than 6 and not 0.
  * Return: Always (back)
  */
 
