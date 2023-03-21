ef7168a5f1a5e13cf6f119a4e470a46ad8342e9f
Rain <rainymorning0000@gmail.com>
2023-03-20 17:54:26 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints all the numbers of base 16 in lowercase
+ * Return: Always (back)
+ */
+
+int main(void)
+
+{
+	int n;
+	char let;
+
+	for (n = 0; n <= 9; n++)
+	putchar(n);
+
+	for (let = 'a'; let <= 'f'; let++)
+	putchar(let);
+	putchar('\n');
+
+	return (0);
+}
