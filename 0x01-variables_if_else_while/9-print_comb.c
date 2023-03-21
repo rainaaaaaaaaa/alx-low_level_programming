7beb1e8c08b101a59a5586af75f8543b68f7e29e
Rain <rainymorning0000@gmail.com>
2023-03-20 18:05:46 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible combinations of single-digit numbers.
+ * Return: Always (back)
+ */
+
+int main(void)
+
+{
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+	putchar(n);
+	{
+	if (n != '9')
+	putchar(',');
+	putchar(' ');
+	}
+	putchar('\n');
+
+	return (0);
+}
