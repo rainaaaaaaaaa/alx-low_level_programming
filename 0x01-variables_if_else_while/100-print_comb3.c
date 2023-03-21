83072c49823fb8e32f3142504b60510732da1461
Rain <rainymorning0000@gmail.com>
2023-03-20 18:26:10 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible different combinations of two digits.
+ * Return: Always (back)
+ */
+
+int main(void)
+
+{
+	int n, m;
+
+	for (n = '0'; n < '9'; n++)
+	{
+	for (m = n + 1; m <= '9'; m++)
+	{
+	if (m != n)
+	{
+	putchar(n);
+	putchar(m);
+	if ( n == '8' && m == '9')
+	continue;
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	}
+	putchar('\n');
+
+	return (0);
+}
