da5023ba5199bf56dd0a6b186b5e1cd6012d3df3
Rain <rainymorning0000@gmail.com>
2023-03-20 19:29:57 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -8,11 +8,11 @@
 int main(void)
 
 {
-        int n;
+	int n;
 
-        for (n = 0; n < 10; n++)
-        putchar(n);
-        putchar('\n');
+	for (n = 0; n < 10; n++)
+	putchar(n);
+	putchar('\n');
 
-        return (0);
+	return (0);
 }
