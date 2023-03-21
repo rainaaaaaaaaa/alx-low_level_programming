b7bb6baf97c9de9b63e213ad786fe207b6c51800
Rain <rainymorning0000@gmail.com>
2023-03-21 14:54:53 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - prints the alphabet in lowercase
+ * Return: Always (0)
+ */
+
+void print_alphabet(void)
+
+{
+	char l;
+
+	for (l = 'a'; l <= 'z'; l++)
+	_putchar(l);
+	_putchar('\n');
+
+	return (0);
+}
