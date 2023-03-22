fe47a73e80b7ff5e07531ee1ef9e29f985a1bcc4
Rain <rainymorning0000@gmail.com>
2023-03-22 03:27:51 -0700

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,7 +2,8 @@
 
 /**
  * _islower - Write a function that checks for lowercase character
- * Return: 1 if character is lowercase, 0 otherwise
+ * @c: the character to be checked
+ * Return: 1 if c is lowercase, 0 otherwise
  */
 
 int _islower(int c)
