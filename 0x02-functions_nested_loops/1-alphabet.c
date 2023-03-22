80527ade8712be30f12e357480bfe231098b214c
Rain <rainymorning0000@gmail.com>
2023-03-22 02:28:52 -0700

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,13 +7,13 @@
 void print_alphabet(void)
 
 {
-    char c = 'a';
+	char c = 'a';
 
-    while (c <= 'z')
-    {
-        _putchar(c);
-        c++;
-    }
+	while (c <= 'z')
+	{
+	_putchar(c);
+	c++;
+	}
 
-    _putchar('\n');
+	_putchar('\n');
 }
