72c02fead8231575c35e5c428f236d7e6e317654
Rain <rainymorning0000@gmail.com>
2023-03-23 09:20:44 -0700

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -6,6 +6,7 @@
 */
 
 int main(void)
+
 {
         int i;
 
@@ -13,10 +14,10 @@ int main(void)
 
         i = 0;
 
-        while (i < 10)
-        {
-                putchar(i);
-        }
+        /*while (i < 10)*/
+        /*{*/
+        /*        putchar(i);*/
+        /*}*/
 
         printf("Infinite loop avoided! \\o/\n");
 
