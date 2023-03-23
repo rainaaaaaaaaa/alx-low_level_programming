e8c9bba23313fcf47fc22e276c7e7fc58c501052
Rain <rainymorning0000@gmail.com>
2023-03-22 17:46:36 -0700

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main"
+
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer,
+ * starting from 00:00 to 23:59
+ */
+
+void jack_bauer(void)
+
+{
+	int hour, minute;
+
+	for (hour = 0; hour < 24; hour++)
+	{
+	// Loop through hours
+	for (minute = 0; minute < 60; minute++)
+	{
+	// Loop through minutes
+	printf("%02d:%02d\n", hour, minute);
+	// Print time in format "hh:mm"
+	}
+	}
+}
