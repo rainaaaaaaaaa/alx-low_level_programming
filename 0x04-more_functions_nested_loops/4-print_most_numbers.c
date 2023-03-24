6c5c9ff78308a18489146db185fb434c3fbec77c
Rain <rainymorning0000@gmail.com>
2023-03-24 05:48:42 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -3,6 +3,7 @@
 /**
  * print_most_numbers - prints the numbers from 0 to 9
  * except 2 and 4
+ * Return: the numbers from 0 to 9
  */
 
 void print_most_numbers(void)
@@ -22,4 +23,5 @@ void print_most_numbers(void)
 	}
 	}
 	_putchar('\n');
+	return (n);
 }
