7b32f090ee316bdf005cb64fe894bc91a767c9d0
Rain <rainymorning0000@gmail.com>
2023-03-27 15:03:25 -0700

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: the string
+ */
+
+int _strlen(char *s)
+
+{
+	int length = 0;
+
+	while(*s++)
+	length++;
+	return (length);
+}
