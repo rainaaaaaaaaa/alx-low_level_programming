bbe5bd96f4c13daef5747517250687135367e48a
Rain <rainymorning0000@gmail.com>
2023-03-27 18:28:35 -0700

    puts half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -17,7 +17,7 @@ void puts_half(char *str)
 	}
 	if (i % 2 == 1)
 	{
-	j = (i - 1)/2;
+	j = (i - 1) / 2;
 	j += 1;
 	}
 	else
@@ -29,4 +29,4 @@ void puts_half(char *str)
 	_putchar(str[j]);
 	}
 	_putchar('\n');
-}	
+}
