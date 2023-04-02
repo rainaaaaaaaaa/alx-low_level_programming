e9942a87217a0f985919f7bbc3f6561272770d53
Rain <rainymorning0000@gmail.com>
2023-04-02 08:56:09 -0700

    FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -5,7 +5,7 @@
  * main - prints the numbers from 1 to 100. But for multiples of three
  * print Fizz and for the multiples of five print Buzz
  * For numbers which are multiples of both three and five print FizzBuzz
- * Return: 0
+ * Return: Always 0.
  */
 
 int main(void)
