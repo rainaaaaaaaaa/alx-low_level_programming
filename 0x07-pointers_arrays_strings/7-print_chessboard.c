79968fd50c92e1f7ed7a94679902a67713b05f0f
Rain <rainymorning0000@gmail.com>
2023-04-04 16:58:43 -0700

    chess

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_chessboard - prints the chessboard
+ * @a: an array
+ */
+
+void print_chessboard(char (*a)[8])
+
+{
+	int x, y;
+
+	for (x = 0; x < 0; x++)
+	{
+	for (x = 0; y < 0; y++)
+	_putchar(a[x][y]);
+	_putchar('\n');
+	}
+}
