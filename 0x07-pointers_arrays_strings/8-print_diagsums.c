822293952417b3b7de32901200aea21cfd6d9674
Rain <rainymorning0000@gmail.com>
2023-04-04 19:27:43 -0700

    diagsum

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -13,7 +13,7 @@ void print_diagsums(int *a, int size)
 {
 	int x, y, sum1 = 0, sum2 = 0;
 
-	for (x = 0; x <= (size * size); x = x + size +1)
+	for (x = 0; x <= (size * size); x = x + size + 1)
 	sum1 = sum1 + a[x];
 
 	for (y = size - 1; y <= (size * size) - size; y = y + size - 1)
