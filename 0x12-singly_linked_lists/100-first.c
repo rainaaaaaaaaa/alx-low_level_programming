673fde4f5da6815aea0c2baade1018f31b001ae7
Rain <rainymorning0000@gmail.com>
2023-04-29 07:13:18 -0700

    b

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -2,6 +2,5 @@
 
 void __attribute__((constructor)) calledFirst()
 {
-	printf("You're beat! and yet, you must allow,
-			\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
 }
