19b737fa4ee8c687b2942b3b1697be3aef8470f4
Rain <rainymorning0000@gmail.com>
2023-04-30 08:44:11 -0700

    o

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -11,17 +11,19 @@ listint_t *find_listint_loop(listint_t *head)
 {
 	listint_t *slow, *fast;
 
-	if (head == NULL || head->next == NULL)
+	if (head == NULL)
 		return (NULL);
 
 	slow = head;
-	fast = head->next;
+	fast = head;
 
 	while (fast != NULL && fast->next != NULL)
 	{
+		slow = slow->next;
+		fast = fast->next->next;
+
 		if (slow == fast)
 		{
-			/* Loop found. Move slow to head and start again. */
 			slow = head;
 			while (slow != fast)
 			{
@@ -30,8 +32,6 @@ listint_t *find_listint_loop(listint_t *head)
 			}
 			return (slow);
 		}
-		slow = slow->next;
-		fast = fast->next->next;
 	}
 
 	return (NULL);
