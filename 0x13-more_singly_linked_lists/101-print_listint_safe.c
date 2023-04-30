d88753884a7eefd2691c64bb96ba2c058a751701
Rain <rainymorning0000@gmail.com>
2023-04-30 07:55:27 -0700

    m

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -13,27 +13,51 @@ size_t print_listint_safe(const listint_t *head)
 	const listint_t *tortoise, *hare;
 	size_t count = 0;
 
-	if (head == NULL)
-		exit(98);
+	tortoise = hare = head;
 
-	tortoise = head;
-	hare = head->next;
-
-	while (hare != NULL && hare < hare->next)
+	while (tortoise != NULL && hare != NULL && hare->next != NULL)
 	{
-		printf("[%p] %d\n", (void *)tortoise, tortoise->n);
 		tortoise = tortoise->next;
 		hare = hare->next->next;
+
+		if (tortoise == hare)
+		{
+			printf("-> [%p] %d\n", (void *)tortoise, tortoise->n);
+			break;
+		}
+
+		printf("[%p] %d\n", (void *)tortoise, tortoise->n);
 		count++;
 	}
 
-	printf("[%p] %d\n", (void *)tortoise, tortoise->n);
-	count++;
-
-	if (hare != NULL)
+	if (tortoise == NULL || hare == NULL || hare->next == NULL)
 	{
-		printf("-> [%p] %d\n", (void *)hare, hare->n);
-		count++;
+		/* no loop */
+		while (head != NULL)
+		{
+			printf("[%p] %d\n", (void *)head, head->n);
+			head = head->next;
+			count++;
+		}
+	}
+	else
+	{
+		/* loop detected */
+		tortoise = head;
+		while (tortoise != hare)
+		{
+			printf("[%p] %d\n", (void *)tortoise, tortoise->n);
+			tortoise = tortoise->next;
+			hare = hare->next;
+		}
+		printf("-> [%p] %d\n", (void *)tortoise, tortoise->n);
+
+		do {
+			tortoise = tortoise->next;
+			count++;
+		} while (tortoise != hare);
+
+		printf("-> [%p] %d\n", (void *)tortoise, tortoise->n);
 	}
 
 	return (count);
