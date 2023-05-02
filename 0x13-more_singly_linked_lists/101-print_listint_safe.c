3d261304023252502ddff6bf5a469d70a977f651
Rain <rainymorning0000@gmail.com>
2023-05-02 16:38:07 -0700

    m

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,63 +1,29 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "lists.h"
 
 /**
  * print_listint_safe - Prints a listint_t linked list.
- * @head: Pointer to the head node of the list.
  *
- * Return: Number of nodes in the list.
+ * @head: A pointer to the head of the listint_t list.
+ *
+ * Return: The number of nodes in the list.
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *tortoise, *hare;
 	size_t count = 0;
+	const listint_t *curr, *next;
 
-	tortoise = hare = head;
-
-	while (tortoise != NULL && hare != NULL && hare->next != NULL)
+	curr = head;
+	while (curr != NULL)
 	{
-		tortoise = tortoise->next;
-		hare = hare->next->next;
-
-		if (tortoise == hare)
-		{
-			printf("-> [%p] %d\n", (void *)tortoise, tortoise->n);
-			break;
-		}
-
-		printf("[%p] %d\n", (void *)tortoise, tortoise->n);
+		next = curr->next;
 		count++;
-	}
-
-	if (tortoise == NULL || hare == NULL || hare->next == NULL)
-	{
-		/* no loop */
-		while (head != NULL)
+		printf("[%p] %d\n", (void *) curr, curr->n);
+		if (next >= curr)
 		{
-			printf("[%p] %d\n", (void *)head, head->n);
-			head = head->next;
-			count++;
+			printf("-> [%p] %d\n", (void *) next, next->n);
+			exit(98);
 		}
-	}
-	else
-	{
-		/* loop detected */
-		tortoise = head;
-		while (tortoise != hare)
-		{
-			printf("[%p] %d\n", (void *)tortoise, tortoise->n);
-			tortoise = tortoise->next;
-			hare = hare->next;
-		}
-		printf("-> [%p] %d\n", (void *)tortoise, tortoise->n);
-
-		do {
-			tortoise = tortoise->next;
-			count++;
-		} while (tortoise != hare);
-
-		printf("-> [%p] %d\n", (void *)tortoise, tortoise->n);
+		curr = next;
 	}
 
 	return (count);
