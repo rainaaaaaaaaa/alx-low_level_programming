ba76df2448205d5795d612d1d6a40c43ace67cc3
Rain <rainymorning0000@gmail.com>
2023-05-03 12:31:46 -0700

    m

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,6 +1,35 @@
 #include "lists.h"
 
 /**
+ * _r - reallocates memory
+ * 
+ * @list: the old list.
+ *
+ * @size: size of the new list.
+ *
+ * @new: new node.
+ *
+ * Return: pointer to the new list.
+ */
+const listint_t **_r(const listint_t **list, size_t size, const listint_t *new)
+{
+	const listint_t **newlist;
+	size_t n;
+
+	newlist = malloc(size * sizeof(listint_t *));
+	if (newlist == NULL)
+	{
+		free(list);
+		exit(98);
+	}
+	for (n = 0; n < size - 1; n++)
+		newlist[n] = list[n];
+	newlist[n] = new;
+	free(list);
+	return (newlist);
+}
+
+/**
  * print_listint_safe - Prints a listint_t linked list.
  *
  * @head: A pointer to the head of the listint_t list.
@@ -9,22 +38,25 @@
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t count = 0;
-	const listint_t *curr, *next;
+	size_t n, count = 0;
+	const listint_t **list = NULL;
 
-	curr = head;
-	while (curr != NULL)
+	while (head != NULL)
 	{
-		next = curr->next;
-		count++;
-		printf("[%p] %d\n", (void *) curr, curr->n);
-		if (next >= curr)
+		for (n = 0; n < count; n++)
 		{
-			printf("-> [%p] %d\n", (void *) next, next->n);
-			exit(98);
+		if (head == list[n])
+		{
+			printf("-> [%p] %d\n", (void *)head, head->n);
+			free(list);
+			return (count);
 		}
-		curr = next;
+		}
+		count++;
+		list = _r(list, count, head);
+		printf("-> [%p] %d\n", (void *)head, head->n);
+		head = head->next;
 	}
-
+	free(list);
 	return (count);
 }
