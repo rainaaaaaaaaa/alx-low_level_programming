5267772f4c69cc42d896d2dcfef506a06626ccb5
Rain <rainymorning0000@gmail.com>
2023-05-03 16:11:18 -0700

    n

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -8,35 +8,35 @@
  */
 size_t free_listint_safe(listint_t **h)
 {
-        size_t count = 0;
-        listint_t *slow, *fast, *temp;
+	size_t count = 0;
+	listint_t *slow, *fast, *temp;
 
-        if (h == NULL || *h == NULL)
-                return (0);
+	if (h == NULL || *h == NULL)
+		return (0);
 
-        slow = *h;
-        fast = (*h)->next;
-        while (fast != NULL && fast < slow)
-        {
-                count++;
-                temp = slow;
-                slow = slow->next;
-                free(temp);
-                fast = fast->next;
-                if (fast != NULL && fast < slow)
-                {
-                        count++;
-                        temp = slow;
-                        slow = slow->next;
-                        free(temp);
-                        fast = fast->next;
-                }
-        }
-        count++;
-        temp = slow;
-        slow = slow->next;
-        free(temp);
-        *h = NULL;
+	slow = *h;
+	fast = (*h)->next;
+	while (fast != NULL && fast < slow)
+	{
+		count++;
+		temp = slow;
+		slow = slow->next;
+		free(temp);
+		fast = fast->next;
+		if (fast != NULL && fast < slow)
+		{
+			count++;
+			temp = slow;
+			slow = slow->next;
+			free(temp);
+			fast = fast->next;
+		}
+	}
+	count++;
+	temp = slow;
+	slow = slow->next;
+	free(temp);
+	*h = NULL;
 
-        return (count);
+	return (count);
 }
