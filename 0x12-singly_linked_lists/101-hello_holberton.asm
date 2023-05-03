386342cf9389b8ecfdfaa5612aed2649af0f8c3a
Rain <rainymorning0000@gmail.com>
2023-05-03 06:23:52 -0700

    101

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,7 +1,18 @@
-#include <stdio.h>
+section .data
+    hello db 'Hello, Holberton', 0   ; null-terminated string
 
-int main(void)
-{
-printf("Hello, Holberton\n");
-return (0);
-}
+section .text
+    global main
+    extern printf
+
+main:
+    push rbp
+    mov rbp, rsp
+
+    lea rdi, [hello]    ; load the address of hello string into rdi
+    xor eax, eax        ; clear eax register
+    call printf         ; call printf function to print hello string
+
+    mov rsp, rbp
+    pop rbp
+    ret
