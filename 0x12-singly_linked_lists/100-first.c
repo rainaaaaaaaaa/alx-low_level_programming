aedf14c3a8d5c7a5e3e19c7c13901b263e815818
Rain <rainymorning0000@gmail.com>
2023-05-03 05:59:43 -0700

    first

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
+#include "lists.h"
 
-void __attribute__((constructor)) calledFirst()
+/* _constructor - executes before main is executed */
+void _constructor(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back!\n");
 }
