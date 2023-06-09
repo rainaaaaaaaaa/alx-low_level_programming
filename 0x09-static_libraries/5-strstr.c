c55b63e7553ff2c25a74d9ca9580b5b01e3c7df1
Rain <rainymorning0000@gmail.com>
2023-06-09 12:06:55 -0700

    libmy

---
+++ 0x09-static_libraries/5-strstr.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *_strstr - locates a substring
+ * @haystack:the string
+ * @needle: the substring
+ * Return: a pointer to the beginning of the located substring
+ * or NULL if the substring is not found
+ */
+
+char *_strstr(char *haystack, char *needle)
+
+{
+	for (; *haystack != '\0'; haystack++)
+	{
+	char *h = haystack;
+	char *n = needle;
+
+	while (*h == *n && *n != '\0')
+	{
+	h++;
+	n++;
+	}
+	if (*n == '\0')
+	return (haystack);
+	}
+	return (NULL);
+}
