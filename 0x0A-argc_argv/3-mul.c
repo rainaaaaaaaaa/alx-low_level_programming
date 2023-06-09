04301e96719bb056f83ec89815e3fe2873784f65
Rain <rainymorning0000@gmail.com>
2023-06-09 13:22:22 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -14,11 +14,12 @@ int main(int argc, char *argv[])
 	int num2 = atoi(argv[2]);
 	int result = num1 * num2;
 
-	if (argc != 2)
+	if (argc != 3)
 	{
 	printf("Error\n");
 	return (1);
 	}
+	else
 	printf("%d\n", result);
 	return (0);
 }
