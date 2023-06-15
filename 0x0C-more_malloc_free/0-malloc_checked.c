c7480bb4c8f8f88ee285db634dc04703297fe0dc
Rain <rainymorning0000@gmail.com>
2023-06-15 16:41:21 -0700

    more

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * *malloc_checked - allocates memory using malloc
+ * @b: the size to allocate
+ * Return: a pointer to the allocated memory
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *a;
+
+	a = malloc(b);
+
+	if (a == NULL)
+	{
+	exit(98);
+	}
+
+	return;
+}
