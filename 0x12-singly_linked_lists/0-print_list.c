5d024a7763129c861173c5b0f8cf96996c351c4e
Rain <rainymorning0000@gmail.com>
2023-06-30 14:02:02 -0700

    p

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,26 +1,57 @@
+#include <stddef.h>  // for size_t
 #include "lists.h"
 
 /**
- * size_t print_list - Write a function that prints all the elements
- * of a list_t list.
+ * print_list - Prints all the elements of a list_t list.
+ * @h: Pointer to the head of the list.
  *
- * Description: singly linked list node structure
+ * Return: The number of nodes.
  */
-
 size_t print_list(const list_t *h)
 {
-	size_t count = 0;
+    size_t count = 0;
 
-	while (h != NULL)
-	{
-		if (h->str != NULL)
-			printf("[%u] %s\n", h->len, h->str);
-		else
-			printf("[0] (nil)\n");
+    while (h != NULL)
+    {
+        if (h->str != NULL)
+        {
+            print_string(h->str);
+            print_char('\n');
+        }
+        else
+        {
+            print_string("[0] (nil)");
+            print_char('\n');
+        }
 
-		h = h->next;
-		count++;
-	}
+        h = h->next;
+        count++;
+    }
 
-	return (count);
+    return count;
 }
+
+/**
+ * print_string - Prints a string.
+ * @str: The string to print.
+ */
+void print_string(const char *str)
+{
+    size_t i = 0;
+
+    while (str[i] != '\0')
+    {
+        print_char(str[i]);
+        i++;
+    }
+}
+
+/**
+ * print_char - Prints a character.
+ * @c: The character to print.
+ */
+void print_char(char c)
+{
+    write(1, &c, 1);
+}
+
