01d8a9d59674f83e11da9ff60799e731f10c2610
Rain <rainymorning0000@gmail.com>
2023-07-27 10:19:33 -0700

    s

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,50 +11,20 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-	char *buffer;
-	ssize_t total_read = 0;
-	ssize_t num_read;
-	ssize_t num_written;
+	ssize_t b;
+	char buffer[READ_BUF_SIZE * 8];
 
-	if (filename == NULL)
+	if (!filename || !letters)
 		return (0);
+
 	fd = open(filename, O_RDONLY);
-	if (fd == -1)
-	{
-		perror("Error opening the file");
-		return (0);
-	}
-	buffer = malloc(letters);
-	if (buffer == NULL)
-	{
-		perror("Memory allocation failed");
-		close(fd);
-		return (0);
-	}
 
-	while (letters > 0)
-	{
-		num_read = read(fd, buffer, letters);
-		if (num_read == -1)
-		{
-			perror("Error reading from the file");
-			free(buffer);
-			close(fd);
-			return (total_read);
-		}
-		if (num_read == 0)
-			break;
-		num_written = write(STDOUT_FILENO, buffer, num_read);
-		if (num_written == -1 || num_written != num_read)
-		{
-			perror("Error writing to STDOUT");
-			close(fd);
-			return (total_read);
-		}
-		total_read += num_read;
-		letters -= num_read;
-	}
-	free(buffer);
+	if (fd == -1)
+		return (0);
+
+	b = read(fd, &buffer[0], letters);
+	b = write(STDOUT_FILENO, &buffer[0], b);
+
 	close(fd);
-	return (total_read);
+	return (b);
 }
