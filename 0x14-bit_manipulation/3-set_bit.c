c62f0fda0bf98ab50d2453e9c8212d4f25032db6
Rain <rainymorning0000@gmail.com>
2023-08-03 16:12:30 -0700

    s

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -9,11 +9,11 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int mask = 1UL << index;
-	*n |= mask;
+	unsigned long int mask;
 
 	if (index >= (sizeof(unsigned long int) * 8))
 		return (-1);
-	else
-		return (1);
+	mask = 1UL << index;
+	*n |= mask;
+	return (1);
 }
