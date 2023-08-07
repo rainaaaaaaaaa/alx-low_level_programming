31b5e0be955b36c281c4615b2fb4d8948e624ea8
Rain <rainymorning0000@gmail.com>
2023-08-07 14:45:07 -0700

    a

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -3,7 +3,11 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stddef.h>
 #include <unistd.h>
+#include <fcntl.h>
+
+#define READ_BUFFER_SIZE 1024
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
